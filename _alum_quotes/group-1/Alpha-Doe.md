46c26671a683121cf73642dbf608d16778de64c8
Vladimir Jimenez <vladimir.jimenez@smgov.net>
2015-12-22 13:37:19 -0800

    Monolithic commit; sorry

---
+++ _alum_quotes/group-1/Alpha-Doe.md
@@ -0,0 +1,7 @@
+---
+name: Alpha Doe
+year: 2014
+group: 1
+---
+
+I'm the alpha doe
---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+Date.prototype.addDays = function(days)
+{
+    var dat = new Date(this.valueOf());
+    dat.setDate(dat.getDate() + days);
+    return dat;
+}
+
+$(document).ready(function () {
+    $.getJSON("/assets/data/quotes.json", function (d) {
+        var quoteCard = Handlebars.compile($("#quote-card-template").html());
+        var compiled = quoteCard({ sources: d });
+
+        $("#quote-carousel").html(compiled)
+    });
+
+    $(".js-event").each(function (e) {
+        var $this = $(this);
+        var now = new Date();
+        var date = new Date($this.data("date")).addDays(1);
+
+        if (date.getTime() <= now) {
+            $this.hide();
+        }
+    });
+});
