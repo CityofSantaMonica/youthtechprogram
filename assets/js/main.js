cd9f1d1aba077745104aa9b0518fd3667a9558c4
allejo <allejo@me.com>
2016-01-03 19:34:53 -0800

    Begin building program stats

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,15 @@ Date.prototype.addDays = function(days)
     var dat = new Date(this.valueOf());
     dat.setDate(dat.getDate() + days);
     return dat;
-}
+};
 
 $(document).ready(function () {
-    $.getJSON("/assets/data/quotes.json", function (d) {
-        var quoteCard = Handlebars.compile($("#quote-card-template").html());
-        var compiled = quoteCard({ sources: d });
-
-        $("#quote-carousel").html(compiled)
-    });
+    //$.getJSON("/assets/data/quotes.json", function (d) {
+    //    var quoteCard = Handlebars.compile($("#quote-card-template").html());
+    //    var compiled = quoteCard({ sources: d });
+    //
+    //    $("#quote-carousel").html(compiled)
+    //});
 
     $(".js-event").each(function (e) {
         var $this = $(this);
