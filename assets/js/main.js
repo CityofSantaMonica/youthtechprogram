4b5f455366ba4c02f3ed025d99cc38634d9759a1
Vladimir Jimenez <vladimir.jimenez@smgov.net>
2016-01-12 11:48:59 -0800

    Updates to upcoming events
    - Make it mobile friendly; the proper way
    - Each event is linked to a picture

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,11 @@ $(document).ready(function () {
 
     $('[data-toggle="tooltip"]').tooltip();
 
+    $('#upcoming-events a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+
     $(".js-event").each(function (e) {
         var $this = $(this);
         var now = new Date();
