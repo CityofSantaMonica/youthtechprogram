398aa087bdcbf46bcc1b880f69f2af42c4c2637f
Vladimir Jimenez <vladimir.jimenez@smgov.net>
2016-03-28 09:45:01 -0700

    Normalize carousel height for partners
    Attempt #2

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,34 @@ Date.prototype.addDays = function(days)
     return dat;
 };
 
+function carouselNormalization() {
+    var items = $('.flex-grid-carousel .item'),
+        heights = [],
+        tallest;
+
+    if (items.length) {
+        function normalizeHeights() {
+            items.each(function() {
+                heights.push($(this).height());
+            });
+            tallest = Math.max.apply(null, heights);
+            items.each(function() {
+                $(this).css('min-height',tallest + 'px');
+            });
+        };
+
+        normalizeHeights();
+
+        $(window).on('resize orientationchange', function () {
+            tallest = 0, heights.length = 0;
+            items.each(function() {
+                $(this).css('min-height','0');
+            });
+            normalizeHeights();
+        });
+    }
+}
+
 $(document).ready(function () {
     $.getJSON(baseurl + "/assets/data/quotes.json", function (d) {
         var now = new Date();
@@ -25,6 +53,8 @@ $(document).ready(function () {
 
         randomSorter();
     });
+
+    carouselNormalization();
 
     $('[data-toggle="tooltip"]').tooltip();
 
